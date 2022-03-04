c913679c836477d997268f3514908a89d748de70
Valdemar-iatneh <avvakumov112003@gmail.com>
2022-03-04 11:56:20 +0300

    Added DBRepository

---
+++ ProjectsProject/ProjectsProject/Database/DBRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+using ProjectsProject.Database.Model;
+
+namespace ProjectsProject.Database
+{
+    class DBRepository
+    {
+        SQLiteConnection db;
+        public DBRepository(string databasePath)
+        {
+            db = new SQLiteConnection(databasePath);
+            db.CreateTable<Image>();
+        }
+
+        public IEnumerable<Image> GetItems()
+        {
+            return db.Table<Image>().ToList();
+        }
+
+        public Image GetItem(int id)
+        {
+            return db.Get<Image>(id);
+        }
+
+        public int DeleteItem(int id)
+        {
+            return db.Delete<Image>(id);
+        }
+
+        public int SaveItem(Image item)
+        {
+            if (item.Id != 0)
+            {
+                db.Update(item);
+                return item.Id;
+            }
+            else
+            {
+                return db.Insert(item);
+            }
+        }
+    }
+}
